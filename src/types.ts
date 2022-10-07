0ba7c6b467bb020bbc53196b03bf5d589df25b55
Laboratorynotices <muenchen.webas@gmail.com>
2022-10-08 00:58:05 +0200

    Передача данных объектами

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+// Данные, полученные от сайта fakestoreapi.com
 export interface IProduct {
 	category: string;
 	description: string;
@@ -5,4 +6,12 @@ export interface IProduct {
 	image: string;
 	price: number;
 	title: string;
+}
+
+// Возможные варианты классов ширины блока
+export enum widthClasses {
+	"h-full" = "h-full",
+	"w-1/3" = "w-1/3",
+	"w-1/2" = "w-1/2",
+	"w-2/3" = "w-2/3",
 }
