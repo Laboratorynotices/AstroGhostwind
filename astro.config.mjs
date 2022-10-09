4a07d6207a004211123a0f532d36003212a1ecbd
Laboratorynotices <muenchen.webas@gmail.com>
2022-10-09 18:09:28 +0200

    Вовзвращение в состояние 11-го коммита. 0ded020

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,4 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://laboratorynotices.github.io',
-  base: '/AstroGhostwind',
 });
