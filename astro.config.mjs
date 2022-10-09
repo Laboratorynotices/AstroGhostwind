d8704c560a1791b41f9f8b72b3e0432b1d02a113
Laboratorynotices <muenchen.webas@gmail.com>
2022-10-09 12:55:08 +0200

    Публикация на pages.github. Попытка 7

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://laboratorynotices.github.io',
-  base: '/AstroGhostwind/dist',
+  base: '/AstroGhostwind',
 });
